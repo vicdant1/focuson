c00747a6ecd30e95efdd069574cff8c734922dd8
pedrolgois <pedro_gois@hotmail.com>
2023-07-10 19:21:12 -0300

    cadastro concluido

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -4,9 +4,11 @@ import Image from "next/image";
 import styles from "./index.module.scss";
 import { Steps } from "@/components/Steps";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Cadastro = () => {
   const [activeStep, setActiveStep] = useState(0);
+  const router = useRouter();
   return (
     <main className={styles.cadastro}>
       <div className={styles.container}>
@@ -30,7 +32,82 @@ const Cadastro = () => {
 
           <a href="/login">Já tem uma conta? Faça login</a>
         </div>
-        <div className={styles.info}></div>
+        <div className={styles.info}>
+          <form>
+            {activeStep == 0 && (
+              <>
+                <div className={styles.formGroup}>
+                  <label>Nome completo</label>
+                  <input type="text" />
+                </div>
+                <div className={styles.formGroup}>
+                  <label>Apelido</label>
+                  <input type="text" />
+                </div>
+                <div className={styles.formGroup}>
+                  <label>Gênero</label>
+                  <select>
+                    <option>Masculino</option>
+                    <option>Feminino</option>
+                  </select>
+                </div>
+
+                <div className={styles.formGroup}>
+                  <label>Data de nascimento</label>
+                  <input type="date" />
+                </div>
+              </>
+            )}
+            {activeStep == 1 && (
+              <>
+                <div className={styles.formGroup}>
+                  <label>E-mail</label>
+                  <input type="text" />
+                </div>
+                <div className={styles.formGroup}>
+                  <label>Senha</label>
+                  <input type="password" />
+                </div>
+                <div className={styles.formGroup}>
+                  <label>Confirme a senha</label>
+                  <input type="password" />
+                </div>
+              </>
+            )}
+            {activeStep == 2 && (
+              <>
+                <div className={styles.formGroup}>
+                  <label>Telefone para contato</label>
+                  <input type="text" />
+                </div>
+                <div className={styles.formGroup}>
+                  <label>Endereço</label>
+                  <input type="password" />
+                </div>
+              </>
+            )}
+          </form>
+          <div className={styles.buttons}>
+            <button
+              disabled={activeStep == 0}
+              onClick={() => activeStep > 0 && setActiveStep(activeStep - 1)}
+            >
+              Anterior
+            </button>
+            <button
+              onClick={() => {
+                if (activeStep < 3) {
+                  setActiveStep(activeStep + 1);
+                } else {
+                  alert("Cadastro concluído");
+                  router.push("/login");
+                }
+              }}
+            >
+              {activeStep == 3 ? "Concluir" : "Próximo"}
+            </button>
+          </div>
+        </div>
       </div>
     </main>
   );
